13949a3aadc43bb56ac31d45cc327e31a66c8df9
Bendasch <benediktscherner@posteo.de>
2021-12-15 17:28:04 +0100

    Day 08 done.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
         Some(day) if day == "05" => day_05::main(),
         Some(day) if day == "06" => day_06::main(),
         Some(day) if day == "07" => day_07::main(),
+        Some(day) if day == "08" => day_08::main(),
         Some(_) => println!("Not implemented."),
         None => println!("Please specify a day"),
     }
