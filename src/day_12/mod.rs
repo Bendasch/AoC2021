9906987117dc6cd2a74d79f57ca322bf32f7f4d3
Bendasch <benediktscherner@posteo.de>
2021-12-28 00:51:09 +0100

    Day 12 done.

--- src/day_12/mod.rs
+++ src/day_12/mod.rs
@@ -11,51 +11,53 @@ pub fn main() {
 fn part_one() -> usize {
     let contents = get_contents("src/day_12/input.txt");
     let connections = collect_connections(contents);
-    let paths: Vec<Path> = find_paths(&connections);
-    for path in paths.iter() {
-        println!("{}", path);
-    }
+    let paths: Vec<Path> = find_paths(&connections, false);
     paths.len()
 }
 
-fn part_two() -> u32 {
-    //let contents = get_contents("src/day_12/input.txt");
-    0
+fn part_two() -> usize {
+    let contents = get_contents("src/day_12/input.txt");
+    let connections = collect_connections(contents);
+    let paths: Vec<Path> = find_paths(&connections, true);
+    paths.len()
 }
 
-fn find_paths(connections: &HashMap<Cave, Vec<Cave>>) -> Vec<Path> {
+fn find_paths(connections: &HashMap<Cave, Vec<Cave>>, twice: bool) -> Vec<Path> {
     let start = Cave::new("start".to_string());
-    let path = vec![start.clone()];
     let mut paths: Vec<Path> = Vec::new();
     let paths_from_start = connections.get(&start).unwrap();
     for cave in paths_from_start.iter() {
-        let mut new_path = path.clone();
-        new_path.push(cave.clone());
-        paths.append(&mut continue_path_from_cave(cave, &new_path, connections));
+        let path = vec![start.clone(), cave.clone()];
+        paths.append(&mut continue_path_from_cave(cave, path, connections, twice));
     }
     paths
 }
 
 fn continue_path_from_cave(
     cave: &Cave,
-    path: &Vec<Cave>,
+    path: Vec<Cave>,
     connections: &HashMap<Cave, Vec<Cave>>,
+    twice: bool,
 ) -> Vec<Path> {
     let mut paths: Vec<Path> = Vec::new();
     let next_caves = connections.get(cave).unwrap();
     for next_cave in next_caves.iter() {
-        if next_cave.size == Size::Small && path.contains(next_cave) {
+        if next_cave.size == Size::Small
+            && path.contains(next_cave)
+            && (!twice || next_cave.id == "start" || Cave::path_has_duplicate(&path))
+        {
             continue;
         }
-        let mut new_path = path.clone();
+        let mut new_path = path.clone().to_vec();
         new_path.push(next_cave.clone());
         if next_cave.id == "end" {
             paths.push(Path::new(new_path));
         } else {
             paths.append(&mut continue_path_from_cave(
                 next_cave,
-                &new_path,
+                new_path,
                 connections,
+                twice,
             ));
         }
     }
@@ -68,16 +70,10 @@ enum Size {
     Big,
 }
 
-#[derive(Debug, Hash, Clone)]
+#[derive(Debug, Hash, PartialEq, Clone)]
 struct Cave {
     id: String,
     size: Size,
-}
-
-impl PartialEq for Cave {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id && self.size == other.size
-    }
 }
 
 impl Eq for Cave {}
@@ -94,27 +90,38 @@ impl Cave {
             size,
         }
     }
+
+    fn path_has_duplicate(path: &[Cave]) -> bool {
+        let mut path_ids: Vec<String> = Vec::new();
+        for cave in path.iter().filter(|c| c.size == Size::Small) {
+            if path_ids.contains(&cave.id) {
+                return true;
+            }
+            path_ids.push(cave.id.clone());
+        }
+        false
+    }
 }
 
 fn collect_connections(contents: String) -> HashMap<Cave, Vec<Cave>> {
     let mut connections = HashMap::<Cave, Vec<Cave>>::new();
     for line in contents.lines() {
         let mut parts = line.split('-');
-        let from = Cave::new(parts.next().unwrap().to_string());
-        let to = Cave::new(parts.next().unwrap().to_string());
-        insert_connection(&from, &to, &mut connections);
-        insert_connection(&to, &from, &mut connections);
+        let a = Cave::new(parts.next().unwrap().to_string());
+        let b = Cave::new(parts.next().unwrap().to_string());
+        insert_connection(&a, &b, &mut connections);
+        insert_connection(&b, &a, &mut connections);
     }
     connections
 }
 
-fn insert_connection(from: &Cave, to: &Cave, connections: &mut HashMap<Cave, Vec<Cave>>) {
-    let mut vec: Vec<Cave> = match connections.get(&from) {
+fn insert_connection(key: &Cave, value: &Cave, connections: &mut HashMap<Cave, Vec<Cave>>) {
+    let mut values: Vec<Cave> = match connections.get(key) {
         Some(vector) => vector.to_vec(),
         None => Vec::<Cave>::new(),
     };
-    vec.push(to.clone());
-    connections.insert(from.clone(), vec);
+    values.push(value.clone());
+    connections.insert(key.clone(), values);
 }
 
 struct Path {
@@ -126,7 +133,7 @@ impl Path {
         let mut path = String::new();
         for (k, cave) in caves.iter().enumerate() {
             if k > 0 {
-                path.push_str(",");
+                path.push(',');
             }
             match cave.size {
                 Size::Big => path.push_str(&cave.id.to_uppercase()),
@@ -139,13 +146,7 @@ impl Path {
 
 impl Display for Path {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
-        let caves: Vec<&str> = self._path.split(',').collect();
-        for (k, cave) in caves.iter().enumerate() {
-            if k > 0 {
-                write!(f, "->")?;
-            }
-            write!(f, "{}", cave)?;
-        }
+        write!(f, "{}", self._path)?;
         Ok(())
     }
 }
