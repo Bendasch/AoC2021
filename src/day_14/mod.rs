c665d505fce382b00e96c8a902ddce86de31651b
Bendasch <benediktscherner@posteo.de>
2022-01-04 23:07:54 +0100

    Day 14 done.

--- src/day_14/mod.rs
+++ src/day_14/mod.rs
@@ -11,31 +11,27 @@ fn part_one() -> u64 {
     let contents = get_contents("src/day_14/input.txt");
     let mut polymer = Polymer::new(contents);
     polymer.iterate(10);
-    let max_count = polymer.counts.iter().max().unwrap();
-    let min_count = polymer.counts.iter().filter(|&v| *v > 0).min().unwrap();
-    max_count - min_count
+    let counts = polymer.get_char_counts();
+    counts.values().max().unwrap() - counts.values().min().unwrap()
 }
 
 fn part_two() -> u64 {
     let contents = get_contents("src/day_14/input.txt");
     let mut polymer = Polymer::new(contents);
     polymer.iterate(40);
-    let max_count = polymer.counts.iter().max().unwrap();
-    let min_count = polymer.counts.iter().min().unwrap();
-    max_count - min_count
+    let counts = polymer.get_char_counts();
+    counts.values().max().unwrap() - counts.values().min().unwrap()
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 struct Key(char, char);
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Rules(HashMap<Key, char>);
-
 #[derive(Debug)]
 struct Polymer {
-    template: Vec<char>,
-    rules: Rules,
-    counts: Vec<u64>,
+    rules: HashMap<Key, char>,
+    counts: HashMap<Key, u64>,
+    first_char: char,
+    last_char: char,
 }
 
 impl Polymer {
@@ -44,85 +40,88 @@ impl Polymer {
         let template = lines.next().unwrap().chars().collect::<Vec<char>>();
         lines.next(); // skip empty line separting template and rules
 
-        let mut rules: Rules = Rules(HashMap::new());
+        let mut rules = HashMap::<Key, char>::new();
         for line in lines {
             let mut rule_parts = line.split("->");
             let mut location = rule_parts.next().unwrap().trim().chars();
             let key = Key(location.next().unwrap(), location.next().unwrap());
             let value = rule_parts.next().unwrap().trim().chars().next().unwrap();
-            rules.0.insert(key, value);
+            rules.insert(key, value);
         }
 
-        let mut counts = vec![0; 10];
-        for ch in template.iter() {
-            add_count(&mut counts, *ch);
+        let mut counts = HashMap::<Key, u64>::new();
+        for k in 1..template.len() {
+            let key = Key(template[k - 1], template[k]);
+            if let Some(count) = counts.get_mut(&key) {
+                *count += 1;
+            } else {
+                counts.insert(key, 1);
+            }
         }
 
         Polymer {
-            template,
             rules,
             counts,
+            first_char: template[0],
+            last_char: template[template.len() - 1],
         }
     }
 
     fn iterate(&mut self, iter: usize) {
-        let mut parents = Key(' ', ' ');
-        for k in 1..self.template.len() {
-            parents.0 = self.template[k - 1];
-            parents.1 = self.template[k];
-            println!("Calculating {}{}...", parents.0, parents.1);
-            self.calc_offspring(&parents, &iter);
-        }
-    }
+        for _ in 0..iter {
+            let keys: HashMap<_, _> = self.counts.clone();
+            for (key, old_count) in keys.iter().filter(|(_, &c)| c > 0) {
+                let rule = self.rules.get(key);
+                if rule.is_none() {
+                    continue;
+                } else {
+                    let count = self.counts.get_mut(key).unwrap();
+                    *count -= old_count;
+                }
 
-    fn calc_offspring(&mut self, parents: &Key, iter: &usize) {
-        match self.add_child(parents) {
-            Some(child) if *iter > 1 => self.calc_offspring_recursive(&child, parents, &(iter - 1)),
-            _ => {}
-        }
-    }
+                let left_key = Key(key.0, *rule.unwrap());
+                if let Some(count) = self.counts.get_mut(&left_key) {
+                    *count += old_count;
+                } else {
+                    self.counts.insert(left_key, *old_count);
+                }
 
-    fn calc_offspring_recursive(&mut self, child: &char, parents: &Key, iter: &usize) {
-        let left_parents = Key(parents.0, *child);
-        match self.add_child(&left_parents) {
-            Some(left_child) if *iter > 1 => {
-                self.calc_offspring_recursive(&left_child, &left_parents, &(iter - 1))
+                let right_key = Key(*rule.unwrap(), key.1);
+                if let Some(count) = self.counts.get_mut(&right_key) {
+                    *count += old_count;
+                } else {
+                    self.counts.insert(right_key, *old_count);
+                }
             }
-            _ => {}
         }
+    }
 
-        let right_parents = Key(*child, parents.1);
-        match self.add_child(&right_parents) {
-            Some(right_child) if *iter > 1 => {
-                self.calc_offspring_recursive(&right_child, &right_parents, &(iter - 1))
+    fn get_char_counts(&self) -> HashMap<char, u64> {
+        let mut char_counts = HashMap::<char, u64>::new();
+        for (key, count) in self.counts.iter() {
+            let left_char = key.0;
+            let right_char = key.1;
+
+            if let Some(left_count) = char_counts.get_mut(&left_char) {
+                *left_count += *count;
+            } else {
+                char_counts.insert(left_char, *count);
             }
-            _ => {}
-        }
-    }
 
-    fn add_child(&mut self, parents: &Key) -> Option<char> {
-        let rule = self.rules.0.get(parents);
-        if rule.is_none() {
-            return None;
+            if let Some(right_count) = char_counts.get_mut(&right_char) {
+                *right_count += *count;
+            } else {
+                char_counts.insert(right_char, *count);
+            }
         }
-        let child = rule.unwrap();
-        add_count(&mut self.counts, *child);
-        return Some(*child);
-    }
-}
 
-fn add_count(counts: &mut Vec<u64>, ch: char) {
-    match ch {
-        'B' => counts[0] += 1,
-        'C' => counts[1] += 1,
-        'F' => counts[2] += 1,
-        'H' => counts[3] += 1,
-        'K' => counts[4] += 1,
-        'N' => counts[5] += 1,
-        'O' => counts[6] += 1,
-        'P' => counts[7] += 1,
-        'S' => counts[8] += 1,
-        'V' => counts[9] += 1,
-        _ => panic!("Unknown char {}", ch),
+        *char_counts.get_mut(&self.first_char).unwrap() += 1;
+        *char_counts.get_mut(&self.last_char).unwrap() += 1;
+
+        for (_, count) in char_counts.iter_mut() {
+            *count /= 2;
+        }
+
+        char_counts
     }
 }
