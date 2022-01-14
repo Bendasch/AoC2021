57e82e3cc2786bd36741abbf9aeeeff9cd735127
Bendasch <benediktscherner@posteo.de>
2022-01-14 15:17:24 +0100

    Barely started day 16.

--- src/day_16/mod.rs
+++ src/day_16/mod.rs
@@ -1,4 +1,4 @@
-//use crate::get_contents;
+use crate::get_contents;
 
 //use std::fmt::{Display, Error, Formatter};
 
@@ -8,11 +8,23 @@ pub fn main() {
 }
 
 fn part_one() -> u32 {
-    //let contents = get_contents("src/day_16/example_01.txt");
+    let contents = get_contents("src/day_16/example_01.txt");
+    let _packets = parse_packets::<u16>(contents);
     0
 }
 
 fn part_two() -> u32 {
     //let contents = get_contents("src/day_16/example_01.txt");
     0
+}
+
+struct Packet<T: Default> {
+    _version: u8,
+    _type_id: u8,
+    _number_of_subpackets: T,
+    _subpackets: Vec<Packet<T>>,
+}
+
+fn parse_packets<T: Default>(_contents: String) -> Vec<Packet<T>> {
+    Vec::<Packet<T>>::new()
 }
